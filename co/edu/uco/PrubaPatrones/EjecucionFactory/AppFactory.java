5615e4d5bc91b6dde11ce4152e5272bf88845fa3
Andres Velez <andresjobpk@gmail.com>
2024-10-06 20:23:06 -0500

    Practica sobre como conectar java a una base a de datos usando la clase Connection y la clase DriverManager y las interfaces Statement, PreparedStatement and Resultset, tambien aprendiedo hacer buenas excepciones ya establesidas o mis propias excepciones

--- co/edu/uco/PrubaPatrones/EjecucionFactory/AppFactory.java
+++ co/edu/uco/PrubaPatrones/EjecucionFactory/AppFactory.java
@@ -4,20 +4,20 @@ import co.edu.uco.PrubaPatrones.EjecucionFactory.InterfaceFactory.InterConexion;
 
 public class AppFactory {
 	
-	public static void main(String[] args) {
-		ConexionFactory factory = new ConexionFactory();
-		
-		InterConexion cx1 = factory.getConexion("ORACLE");
-		cx1.conectar();
-		cx1.desconectar();
-		
-		InterConexion cx2 = factory.getConexion("MYSQL");
-		cx2.conectar();
-		cx2.desconectar();
-		
-		InterConexion cx3 = factory.getConexion("H2");
-		cx3.conectar();
-		cx3.desconectar();
-	}
+//	public static void main(String[] args) {
+//		ConexionFactory factory = new ConexionFactory();
+//		
+//		InterConexion cx1 = factory.getConexion("ORACLE");
+//		cx1.conectar();
+//		cx1.desconectar();
+//		
+//		InterConexion cx2 = factory.getConexion("MYSQL");
+//		cx2.conectar();
+//		cx2.desconectar();
+//		
+//		InterConexion cx3 = factory.getConexion("H2");
+//		cx3.conectar();
+//		cx3.desconectar();
+//	}
 
 }
--- co/edu/uco/PrubaPatrones/EjecucionMainDAO/App.java
+++ co/edu/uco/PrubaPatrones/EjecucionMainDAO/App.java
@@ -6,11 +6,11 @@ import co.edu.uco.PrubaPatrones.EjecucionMainDAO.ModeloDAO.Medico;
 import co.edu.uco.PrubaPatrones.EjecucionMainDAO.ModeloDAO.Persona;
 
 public class App {
-	public static void main(String[] args) {
-//		CRUD<Medico> daoP = new MedicoDAOImpl();
-//		daoP.listarTodos().forEach(x -> System.out.print(x.getNombres() + x.getId()));
-		
-	}
+//	public static void main(String[] args) {
+////		CRUD<Medico> daoP = new MedicoDAOImpl();
+////		daoP.listarTodos().forEach(x -> System.out.print(x.getNombres() + x.getId()));
+//		
+//	}
 	
 
 }
