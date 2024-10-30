b2e20720c0ed9d68fc38e65f50d0263bbab4ad3c
Andres Velez <andresjobpk@gmail.com>
2024-10-30 14:04:04 -0500

    practicando con postman GET POST DELETE UPDATE

---
+++ ucobet/src/main/java/co/edu/uco/ucobet/initializer/domain/City.java
@@ -0,0 +1,27 @@
+package co.edu.uco.ucobet.initializer.domain;
+
+
+public class City {
+	private String nombre;
+	private long id;
+	
+	
+	public City(String nombre, long id) {
+		this.nombre = nombre;
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	
+}
